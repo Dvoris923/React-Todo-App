add3acf894f11b101fffac7aa5bd365f620faa9e
Саша <martinecsasa5@gmail.com>
2026-02-18 19:38:12 +0200

    <your commit message>

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,94 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
-import './App.scss';
-
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import { UserWarning } from './UserWarning';
+import { USER_ID } from './api/todos';
+import { ErrorNotification } from './components/ErrorNotification';
+import { useTodosManager } from './hooks/useTodosManager';
+import { TodosFilter } from './components/Footer/TodosFilter';
+import { TodosCounter } from './components/Footer/TodosCounter';
+import { ClearCompletedButton } from './components/Footer/ClearCompletedButton';
+import { TodoList } from './components/TodoList/TodoList';
+import { NewTodoForm } from './components/header/NewTodoForm';
+import { Button } from './components/header/Button';
 
 export const App: React.FC = () => {
+  const {
+    todos,
+    errorMessage,
+    setErrorMessage,
+    filter,
+    setFilter,
+    todosCounter,
+    preparedTodos,
+    handleAddTodo,
+    handlDeleteTodo,
+    handleClearCompleted,
+    tempTodo,
+    inputRef,
+    setShouldFocus,
+    loadingId,
+    isDisabled,
+    allCompleted,
+    loading,
+    handlToggleTodo,
+    handlToggleAll,
+    handleUpdateTodo,
+  } = useTodosManager();
+
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <header className="todoapp__header">
+          {todos.length > 0 && (
+            <>
+              <Button
+                allCompleted={allCompleted}
+                onToggleAll={handlToggleAll}
+              />
+            </>
+          )}
+          <NewTodoForm
+            onAddTodo={handleAddTodo}
+            inputRef={inputRef}
+            loading={loading}
+          />
+        </header>
+
+        <TodoList
+          todos={preparedTodos}
+          loadingIds={loadingId}
+          onDelete={handlDeleteTodo}
+          tempTodo={tempTodo}
+          onToggle={handlToggleTodo}
+          onUpdate={handleUpdateTodo}
+        />
+        {todos.length > 0 && (
+          <footer className="todoapp__footer" data-cy="Footer">
+            <TodosCounter count={todosCounter} />
+
+            <TodosFilter filter={filter} setFilter={setFilter} />
+
+            <ClearCompletedButton
+              onClearCompleted={handleClearCompleted}
+              isDisabled={isDisabled}
+            />
+          </footer>
+        )}
+      </div>
+      <ErrorNotification
+        errorMessage={errorMessage}
+        onClose={() => {
+          setErrorMessage('');
+          setShouldFocus(true);
+        }}
+      />
     </div>
   );
 };
